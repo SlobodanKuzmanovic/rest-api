0e1d793d345c55f05bba90d40ffc6dd6f397a201
Slobodan Kuzmanovic <slobodan.kuzmanovic@transcore.com>
2021-09-17 20:47:34 +0200

    logs

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"rest/models"
 	"rest/routes"
 
@@ -11,11 +12,15 @@ import (
 
 func main() {
 	var err error
+	log.Printf("Try open Connection")
 	models.DB, err = sql.Open("mysql", "b9025434f58ccf:b9025434f58ccf@/eu-cdbr-west-01.cleardb.com")
-
+	log.Printf("BBBBBBBBBBBBBBBBBB")
 	if err != nil {
+		log.Printf(err.Error())
+
 		panic(err.Error())
 	}
+	log.Printf("SSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
 	fmt.Println("Successfully")
 	defer models.DB.Close()
 
