de17eca7362238fe432a9e19dd39d8f7bc6254a3
Slobodan Kuzmanovic <slobodan.kuzmanovic@transcore.com>
2021-09-17 21:46:01 +0200

    logs

--- models/userDb.go
+++ models/userDb.go
@@ -106,6 +106,17 @@ func AllUsers() ([]User, error) {
 
 	log.Printf("All users na DB fajlu")
 
+	errDB := DB.Ping()
+	if errDB != nil {
+		var errOpenAgain error
+		log.Printf("ALL USERS DB.Ping() NECE")
+		DB, errOpenAgain = sql.Open("mysql", "b9025434f58ccf:b9025434f58ccf@eu-cdbr-west-01.cleardb.com/heroku_ecd70f5db7afe86")
+		if errOpenAgain != nil {
+			log.Printf("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOpet neće")
+		}
+
+	}
+
 	results, err := DB.Query("SELECT Pk_UserId, Name, Surname, Email, Password from Users")
 	if err != nil {
 		log.Printf("Get users NECE")
